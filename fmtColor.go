674334bc8159bf5f82556d63ebd47767c0a8473f
Luca Morgado <luca.morgado.pro@gmail.com>
2022-10-12 12:28:09 +0200

    added random color prints

--- fmtColor.go
+++ fmtColor.go
@@ -3,11 +3,25 @@ package fmtColor
 import (
 	"fmt"
 	"image/color"
+	"math/rand"
+	"time"
 )
 
 func ToRGB(c color.Color) (uint8, uint8, uint8) {
 	r, g, b, _ := c.RGBA()
 	return uint8(r), uint8(g), uint8(b)
+}
+
+func RandRGB(seed ...int64) (uint8, uint8, uint8) {
+	if len(seed) > 0 {
+		rand.Seed(seed[0])
+	} else {
+		rand.Seed(time.Now().UnixNano())
+	}
+	r := uint8(rand.Intn(255))
+	g := uint8(rand.Intn(255))
+	b := uint8(rand.Intn(255))
+	return r, g, b
 }
 
 func PrintRGB(r uint8, g uint8, b uint8, values ...any) {
@@ -25,4 +39,14 @@ func PrintlnRGB(r uint8, g uint8, b uint8, values ...any) {
 func PrintlnColor(c color.Color, values ...any) {
 	r, g, b := ToRGB(c)
 	PrintlnRGB(r, g, b, values...)
+}
+
+func PrintRandColor(values ...any) {
+	r, g, b := RandRGB()
+	PrintRGB(r, g, b, values...)
+}
+
+func PrintlnRandColor(values ...any) {
+	PrintRandColor(values...)
+	fmt.Println()
 }
